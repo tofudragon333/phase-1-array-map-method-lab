47c98ca819e6d6eae6a3566186b6903ba8aa7cb9
Ashley Wong <tofudragon333@gmail.com>
2022-05-18 18:41:59 -0400

    commit tree 9969d167cd2984bb3df71d752289cecf587c1d98

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () => tutorials.map(title => {
+    const individualWords = title.split(" ");
+    const capitalizedWords = individualWords.map(capitalizeFirstLetter);
+    return capitalizedWords.join(" ");
+});
+
+console.log(titleCased);
+// take each element in an array and iterate it so you can see each title separately.
+
+// const titlesAsWordsArray = tutorials.map(title => title.split(' '));
+
+// console.log(titlesAsWordsArray);
+
+
+// for each iteration, which is one individual title, iterate it again so we have an array of words.
+
+// apply charAt() for each word in the 2nd order iteration level
+
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+  
 }
+
+// const titlesAsWords = titlesAsWordsArray.map(words => {
+//   // console.log(words);
+//   const capitalizedWords = words.map(capitalizeFirstLetter);
+//   return capitalizedWords.join(' ');
+// });
+
+
+// join everything back together into the titleCased variable
+
